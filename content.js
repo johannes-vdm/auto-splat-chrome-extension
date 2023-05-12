09d351e50a20e9cbbdbced41d292b70bfea75da6
Johannes van der Merwe <johannes.vandermerwe@adaptit.com>
2023-05-12 12:27:00 +0200

    create updated feature, displays in body

--- content.js
+++ content.js
@@ -1,9 +1,12 @@
 const updateBtn = document.querySelector("#update");
+const eventList = document.createElement("ul");
+document.body.appendChild(eventList);
 
 function clickUpdateBtn() {
   if (updateBtn) {
     updateBtn.click();
     console.log("CLICKED");
+    addEventToList("CLICKED");
   }
 }
 
@@ -13,6 +16,14 @@ function overrideAlert() {
     clickUpdateBtn();
     return true;  // Return true to automatically accept the alert
   }
+}
+
+function addEventToList(event) {
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+  const listItem = document.createElement("li");
+  listItem.innerText = `${time} - ${event}`;
+  eventList.appendChild(listItem);
 }
 
 // Call the function after a short delay when the page is loaded
@@ -25,20 +36,3 @@ setInterval(clickUpdateBtn, 30 * 60 * 1000);
 
 // Add an event listener to the document object for the 'DOMContentLoaded' event
 document.addEventListener('DOMContentLoaded', overrideAlert);
-chrome.windows.create({
-  type: 'popup',
-  url: '',
-  width: 400,
-  height: 200,
-  top: screen.availHeight / 2 - 100,
-  left: screen.availWidth / 2 - 200
-}, function (window) {
-  chrome.notifications.create({
-    type: 'basic',
-    iconUrl: 'icon.png',
-    title: 'Page loaded!',
-    message: 'The page has finished loading.',
-    requireInteraction: true
-  })
-
-})
